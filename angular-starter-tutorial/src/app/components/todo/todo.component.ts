010d069e16867e07249215e50fbac9fbb94eb742
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-03-26 21:03:47 +0100

    todo app implementation done

--- angular-starter-tutorial/src/app/components/todo/todo.component.ts
+++ angular-starter-tutorial/src/app/components/todo/todo.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent implements OnInit {
   todos!:Todo[];
+  inputTodo:string = "";
 
   ngOnInit(): void {
     this.todos = [
@@ -35,10 +37,17 @@ export class TodoComponent implements OnInit {
 
   toggleDone (id:number) {
     console.log ("id TEST: " + id);
-    // this.todos.map((item, index) => {
-    //     if(index == id) item.completed = !item.completed;
+    this.todos.map((item, index) => {
+        if(index == id) item.completed = !item.completed;
+    })
+  }
 
-    //     return item
-    // })
+  deleteTask (id:number) {
+    this.todos.splice(id, 1);
+  }
+
+  addTask () {
+    this.todos.push({content: this.inputTodo, completed: false});
+    this.inputTodo="";
   }
 }
