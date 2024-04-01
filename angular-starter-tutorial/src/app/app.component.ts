5c3a15baa8e04057a62063cdc222f164283fa6e9
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-04-01 18:59:53 +0200

    add form group with input fields and validators

--- angular-starter-tutorial/src/app/app.component.ts
+++ angular-starter-tutorial/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './components/todo/todo.component';
+import { MyFormComponent } from './components/my-form/my-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TodoComponent],
+  imports: [RouterOutlet,TodoComponent, MyFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angular-starter-tutorial/src/app/components/my-form/my-form.component.ts
+++ angular-starter-tutorial/src/app/components/my-form/my-form.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './my-form.component.html',
   styleUrl: './my-form.component.scss'
 })
 export class MyFormComponent {
+  userForm: FormGroup;
+  isFormValid: boolean;
+
+  constructor () {
+    this.userForm = new FormGroup({
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      // city: new FormControl(""),
+      // state: new FormControl(""),
+      // zipcode: new FormControl(""),
+      // isAgree: new FormControl(false, [Validators.required])
+    });
+
+    this.isFormValid = false;
+  }
+
+  onSubmit() {
+    const isFormValid = this.userForm.valid;
+    console.log("\n\ntets: " + this.userForm.valid)
+    console.log(this.userForm.value)
+    if (!isFormValid) {
+      this.isFormValid = true
+    }
+  }
 
 }
